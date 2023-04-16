2d32c1aae802f2ce53e427860a4e4ad6f5183da0
E Luckie <eluckie.d@gmail.com>
2023-04-15 22:34:29 -0600

    add img & components directories and all components files

---
+++ src/components/ExamplePropTypes.js
@@ -0,0 +1,19 @@
+// MyExampleComponent.propTypes = {
+//   exampleArray: PropTypes.array,
+//   exampleBoolean: PropTypes.bool,
+//   exampleFunction: PropTypes.func,
+//   exampleNumber: PropTypes.number,
+//   exampleObject: PropTypes.object,
+//   exampleString: PropTypes.string,
+//   exampleSymbol: PropTypes.symbol,
+//   exampleReactElement: PropTypes.element,
+// }
+
+// ...
+//   exampleArrayOfNumbers: PropTypes.arrayOf(PropTypes.number),
+//   exampleArrayOfStrings: PropTypes.arrayOf(PropTypes.string),
+// ...
+
+// ...
+//   exampleClassTypeProp: PropTypes.instanceOf(ExampleClassName),
+// ...
